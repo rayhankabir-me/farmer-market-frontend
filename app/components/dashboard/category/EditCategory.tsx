8be098248d8b9efcc460592ea14a06102a4e891b
rayhankabir-me <devrayhankabir@gmail.com>
2024-09-30 00:41:37 +0600

    category edit issue fixed

--- app/components/dashboard/category/EditCategory.tsx
+++ app/components/dashboard/category/EditCategory.tsx
@@ -1,38 +1,47 @@
 "use client";
 
 import axios from "axios";
-// import Cookies from "js-cookie";
+import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-export default function EditCategory({ id }) {
+export default function EditPrductData({ id }) {
   const [successMessage, setSuccessMessage] = useState("");
-  //   const [accessToken, setAccessToken] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
   const [currentId, setCurrentId] = useState(id);
 
-  //setting category data in state
-  const [categoryData, setCateoryData] = useState("");
+  //setting user data in state
+  const [categoryData, setCategoryData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  //getting the access token for user
-  //   useEffect(() => {
-  //     const access_token = Cookies.get("access_token");
-  //     setAccessToken(access_token);
-  //   }, []);
+  //   getting the access token for user
+  useEffect(() => {
+    const access_token = Cookies.get("Token");
+    setAccessToken(access_token);
+  }, []);
 
   //getting the single category data
   useEffect(() => {
     async function fetchCategoryData() {
+      if (!accessToken) return;
+
       try {
         const response = await axios.get(
-          process.env.NEXT_PUBLIC_BACKEND_API + "/api/categories/" + currentId
+          process.env.NEXT_PUBLIC_BACKEND_API + "/api/categories/" + currentId,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
         );
-        setCateoryData(response.data);
+        setCategoryData(response.data);
+        setIsLoading(false);
       } catch (error) {
-        console.log(error);
+        setIsLoading(false);
       }
     }
 
     fetchCategoryData();
-  }, [currentId]);
+  }, [currentId, accessToken]);
 
   const {
     register,
@@ -48,22 +57,51 @@ export default function EditCategory({ id }) {
         formData,
         {
           headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InJheWhhbiIsInJvbGUiOiJBZG1pbiIsImVtYWlsIjoicmF5aGFua2FiaXIud3BAZ21haWwuY29tIiwiVXNlcklkIjoiMSIsIm5iZiI6MTcyNzQ0MTQxMSwiZXhwIjoxNzI3NDQzMjExLCJpYXQiOjE3Mjc0NDE0MTEsImlzcyI6IkZhcm1lck1hcmtldCIsImF1ZCI6IlVzZXJzIn0.B_qrL3XF36-7-aLKd3_v9UayjTIzRfnNGAZgzwB-goQ`,
+            Authorization: `Bearer ${accessToken}`,
           },
         }
       );
 
       if (response.status === 200) {
-        setSuccessMessage("Category updated successfully!");
+        setSuccessMessage("Category updated successfully....!");
       }
     } catch (error) {
-      const errorMessage = error.response?.data?.message || "An error occurred";
+      const errorMessage = error.response?.data?.Message || "An error occurred";
       setError("root.random", {
         message: errorMessage,
         type: "random",
       });
     }
   };
+
+  if (isLoading) {
+    return (
+      <button
+        disabled
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+      >
+        <svg
+          aria-hidden="true"
+          role="status"
+          className="inline w-4 h-4 me-3 text-white animate-spin"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="#E5E7EB"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentColor"
+          />
+        </svg>
+        Loading...
+      </button>
+    );
+  }
 
   return (
     <div className="p-4 sm:ml-64">
@@ -82,7 +120,7 @@ export default function EditCategory({ id }) {
                   {...register("Name", {
                     required: "Your must fill Category Name...",
                   })}
-                  type="Name"
+                  type="text"
                   id="Name"
                   name="Name"
                   defaultValue={categoryData.Name}
@@ -95,6 +133,7 @@ export default function EditCategory({ id }) {
                   </p>
                 )}
               </div>
+
               <button
                 type="submit"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
